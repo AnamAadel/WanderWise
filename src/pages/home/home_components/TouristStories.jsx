3275e4f3b89403b840475df2df87657e3a82a51d
Anam Aadel <rruholislam1998@gmail.com>
2023-12-12 13:26:05 +0600

    show only 6 stories in the home page

--- src/pages/home/home_components/TouristStories.jsx
+++ src/pages/home/home_components/TouristStories.jsx
@@ -10,7 +10,7 @@ function TouristStories() {
 <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
 <h2 className='font-thin text-5xl text-neutral capitalize mb-10 font-permanent relative text-center'>Tourist Stories</h2>
   <div className="grid lg:grid-cols-2 gap-6">
-  {storyData.map(story => (
+  {storyData.slice(0, 6).map(story => (
     <StoryCard story={story} key={story._id} />
 
   ))}
